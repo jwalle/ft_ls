c4d36aaa0e6647a7ea5f61669085ef519e648dbb
jwalle1 <emploi.walle@gmail.com>
2015-01-28 10:31:45 +0000

    misc

---
+++ misc.c
@@ -0,0 +1,45 @@
+/*- HEADER -*/
+
+#include "ft_ls.h"
+
+int		nblen(int len)
+{
+	int		i;
+	
+	i = (len <= 0 ? 1 : 0);
+	while (len)
+	{
+		len = len/10;
+		++i;
+	}
+	//printf("i get len = %d\n", i);
+	return(i);
+}
+
+void	ft_putnstr(int max, char *str)
+{
+	int i;
+	
+	i = 0;
+	while ((int)(ft_strlen(str) + i) < max)
+	{
+		ft_putchar(' ');
+		i++;
+	}
+	ft_putstr(str);
+	ft_putchar(' ');
+}
+
+void	ft_putnnbr(int max, int info)
+{
+	int	i;
+	
+	i = 0;
+	while ((nblen(info) + i) < max)
+	{
+		ft_putchar(' ');
+		i++;
+	}
+	ft_putnbr(info);
+	ft_putchar(' ');
+}
